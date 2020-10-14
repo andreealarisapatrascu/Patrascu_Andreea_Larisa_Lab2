b4e44d7570ac58d9272ceb80d42ba0887ce91065
andreealarisapatrascu <patrascu_larisa98@yahoo.com>
2020-10-14 20:09:34 +0300

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Patrascu_Andreea_Larisa_Lab2.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}

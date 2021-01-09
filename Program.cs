da07173b593337a6ece45c0372b05cdd6738234a
andreealarisapatrascu <58134517+andreealarisapatrascu@users.noreply.github.com>
2021-01-09 20:05:42 +0200

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -6,31 +6,14 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
-using Microsoft.Extensions.DependencyInjection;
-using Patrascu_Andreea_Larisa_Lab2.Data;
 
-namespace Patrascu_Andreea_Larisa_Lab2
+namespace LibraryWebAPI
 {
     public class Program
     {
         public static void Main(string[] args)
         {
-            var host = CreateHostBuilder(args).Build();
-            using (var scope = host.Services.CreateScope())
-            {
-                var services = scope.ServiceProvider;
-                try
-                {
-                    var context = services.GetRequiredService<LibraryContext>();
-                    DbInitializer.Initialize(context);
-                }
-                catch (Exception ex)
-                {
-                    var logger = services.GetRequiredService<ILogger<Program>>();
-                    logger.LogError(ex, "An error occurred while seeding the database.");
-                }
-            }
-            host.Run();
+            CreateHostBuilder(args).Build().Run();
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
